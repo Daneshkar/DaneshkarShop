3bd19ef826341c5e070aa318b12b3acbbddb7086
parsapanahpoor <parsapanahpoor77@gmail.com>
2024-03-11 20:48:41 +0330

    Add IoC Layer

---
+++ DaneshkarShop/DaneshkarShop.IoC/DependencyContainer.cs
@@ -0,0 +1,26 @@
+﻿using DaneshkarShop.Application.Services.Implementation;
+using DaneshkarShop.Application.Services.Interface;
+using DaneshkarShop.Data.Repositories;
+using DaneshkarShop.Domain.IRepositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DaneshkarShop.IoC;
+
+public class DependencyContainer
+{
+    public static void RegisterServices(IServiceCollection services)
+    {
+        // Add services to the container.
+        services.AddScoped<IUserService, UserService>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IRoleRepository, RoleRepository>();
+        services.AddScoped<IRoleService, RoleService>();
+
+        services.AddScoped<IContactUsRepository, ContactUsRepository>();
+        services.AddScoped<IContactUsService, ContactUsService>();
+
+        //Product Categories
+        services.AddScoped<IProductCategoryRepository, ProductCategoryRepository>();
+        services.AddScoped<IProductCategoryService, ProductCategoryService>();
+    }
+}
--- DaneshkarShop/DaneshkarShop.Presentation/Program.cs
+++ DaneshkarShop/DaneshkarShop.Presentation/Program.cs
@@ -3,98 +3,98 @@ using DaneshkarShop.Application.Services.Interface;
 using DaneshkarShop.Data.AppDbContext;
 using DaneshkarShop.Data.Repositories;
 using DaneshkarShop.Domain.IRepositories;
+using DaneshkarShop.IoC;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
 
-namespace DaneshkarShop.Presentation
+namespace DaneshkarShop.Presentation;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        #region Builder
+
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllersWithViews();
+
+        #region Context
+
+        builder.Services.AddDbContext<DaneshkarDbContext>(options =>
         {
-            #region Builder
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DaneshkarDbContextConnectionString"));
+        });
 
-            var builder = WebApplication.CreateBuilder(args);
+        #endregion
 
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
-            builder.Services.AddScoped<IUserService, UserService>();
-            builder.Services.AddScoped<IUserRepository, UserRepository>();
-            builder.Services.AddScoped<IRoleRepository, RoleRepository>();
-            builder.Services.AddScoped<IRoleService, RoleService>();
+        #region Authentication
 
-            builder.Services.AddScoped<IContactUsRepository, ContactUsRepository>();
-            builder.Services.AddScoped<IContactUsService, ContactUsService>();
-
-            //Product Categories
-            builder.Services.AddScoped<IProductCategoryRepository, ProductCategoryRepository>();
-            builder.Services.AddScoped<IProductCategoryService, ProductCategoryService>();
-
-            #region Context
-
-            builder.Services.AddDbContext<DaneshkarDbContext>(options =>
+        builder.Services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        })
+            // Add Cookie settings
+            .AddCookie(options =>
             {
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DaneshkarDbContextConnectionString"));
+                options.LoginPath = "/Account/Login";
+                options.LogoutPath = "/Logout";
+                options.ExpireTimeSpan = TimeSpan.FromDays(30);
             });
 
-            #endregion
+        #endregion
 
-            #region Authentication
+        #region IoC
 
-            builder.Services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-            })
-                // Add Cookie settings
-                .AddCookie(options =>
-                {
-                    options.LoginPath = "/Account/Login";
-                    options.LogoutPath = "/Logout";
-                    options.ExpireTimeSpan = TimeSpan.FromDays(30);
-                });
+        RegisterServices(builder.Services);
 
-            #endregion
+        #endregion
 
-            var app = builder.Build();
+        var app = builder.Build();
 
-            #endregion
+        #endregion
 
-            #region App Services
+        #region App Services
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "MyAreas",
-                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
-                );
-                endpoints.MapControllerRoute(
-                    name: "Default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}"
-                );
-            });
-
-            app.Run();
-
-            #endregion
-
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
         }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "MyAreas",
+                pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+            );
+            endpoints.MapControllerRoute(
+                name: "Default",
+                pattern: "{controller=Home}/{action=Index}/{id?}"
+            );
+        });
+
+        app.Run();
+
+        #endregion
+
+    }
+
+    public static void RegisterServices(IServiceCollection services)
+    {
+        DependencyContainer.RegisterServices(services);
     }
 }
